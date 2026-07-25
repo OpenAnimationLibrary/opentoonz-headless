af1b041af36ca894a8a2b8ff3faca0b6666eeff8
Rodney <rodney.baker@gmail.com>
2026-07-24 22:55:19 -0500

    Add Palette load and save bindings

--- toonz/sources/include/toonz/scriptbinding_palette.h
+++ toonz/sources/include/toonz/scriptbinding_palette.h
@@ -4,7 +4,11 @@
 #define SCRIPTBINDING_PALETTE_H
 
 #include "toonz/scriptbinding.h"
+#include "toonz/scriptbinding_files.h"
 #include "tpalette.h"
+#include "tstream.h"
+#include "tconvert.h"
+#include "tsystem.h"
 
 namespace TScriptBinding {
 
@@ -20,6 +24,96 @@ public:
 
   WRAPPER_STD_METHODS(Palette)
   Q_INVOKABLE QScriptValue toString();
+
+  // Palette file I/O (.tpl)
+  Q_INVOKABLE QScriptValue save(const QScriptValue &pathArg) {
+    TFilePath fp;
+    QScriptValue err = checkFilePath(context(), pathArg, fp);
+    if (err.isError()) return err;
+
+    if (fp.getType() != "tpl") {
+      return context()->throwError(
+          tr("Palette files must use the .tpl extension: %1")
+              .arg(pathArg.toString()));
+    }
+
+    try {
+      TSystem::touchParentDir(fp);
+
+      TOStream os(fp);
+      if (!os) {
+        return context()->throwError(
+            tr("Could not open palette for writing: %1")
+                .arg(pathArg.toString()));
+      }
+
+      std::map<std::string, std::string> attributes;
+      attributes["name"] = ::to_string(m_palette->getGlobalName());
+      os.openChild("palette", attributes);
+      m_palette->saveData(os);
+      os.closeChild();
+      m_palette->setDirtyFlag(false);
+
+      return context()->thisObject();
+    } catch (...) {
+      return context()->throwError(
+          tr("Exception writing palette %1").arg(pathArg.toString()));
+    }
+  }
+
+  Q_INVOKABLE QScriptValue load(const QScriptValue &pathArg) {
+    TFilePath fp;
+    QScriptValue err = checkFilePath(context(), pathArg, fp);
+    if (err.isError()) return err;
+
+    if (fp.getType() != "tpl") {
+      return context()->throwError(
+          tr("Palette files must use the .tpl extension: %1")
+              .arg(pathArg.toString()));
+    }
+
+    if (!TSystem::doesExistFileOrLevel(fp)) {
+      return context()->throwError(
+          tr("Palette file does not exist: %1").arg(pathArg.toString()));
+    }
+
+    TPalette *loadedPalette = nullptr;
+    try {
+      TIStream is(fp);
+      if (!is) {
+        return context()->throwError(
+            tr("Could not open palette for reading: %1")
+                .arg(pathArg.toString()));
+      }
+
+      std::string tagName;
+      if (!is.matchTag(tagName) || tagName != "palette") {
+        return context()->throwError(
+            tr("File is not a valid OpenToonz palette: %1")
+                .arg(pathArg.toString()));
+      }
+
+      std::string globalName;
+      is.getTagParam("name", globalName);
+
+      loadedPalette = new TPalette();
+      loadedPalette->loadData(is);
+      loadedPalette->setGlobalName(::to_wstring(globalName));
+      is.matchEndTag();
+      loadedPalette->setPaletteName(fp.getWideName());
+      loadedPalette->setDirtyFlag(false);
+
+      loadedPalette->addRef();
+      if (m_palette) m_palette->release();
+      m_palette = loadedPalette;
+
+      return context()->thisObject();
+    } catch (...) {
+      if (loadedPalette) delete loadedPalette;
+      return context()->throwError(
+          tr("Exception reading palette %1").arg(pathArg.toString()));
+    }
+  }
 
   // Color management (solid colors)
   Q_INVOKABLE QScriptValue addColor(int r, int g, int b, int a = 255);
